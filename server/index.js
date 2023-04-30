31ef78c44720f4754394644343734b1ca57d3c7a
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-04-30 20:36:27 +0900

    パスワードの暗号化

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const CryptoJS = require('crypto-js');
+const User = require('./src/v1/models/user');
 const app = express();
-const PORT = 5000;
+const PORT = 5050;
 require('dotenv').config();
 
 // DB接続
@@ -13,6 +15,17 @@ try {
 }
 
 // ユーザ新規登録API
+app.post('/register', async (req, res) => {
+  // パスワードの受け取り
+  const password = req.body.password;
+
+  try {
+    // パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    // ユーザの新規登録
+    const user = await User.create(req, res);
+  } catch {}
+});
 
 // ユーザログイン用API
 
